6c8d6c63abf23502ecab6472a877d8fef460d8b3
Lunkn <phelsuma_66@hotmail.com>
2015-04-11 10:49:31 +0200

    Added Tiger Big Texture and created attacks

--- src/core/gameObjects/Player.h
+++ src/core/gameObjects/Player.h
@@ -10,5 +10,17 @@ public:
 
 	glm::vec2& GetPosition();
 private:
+
 	const float MOVEMENT_SPEED = 4.0f;
+
+	void CheckAttack(float dt);
+	void Maul();
+	void Pounce();
+
+	float m_Damage;
+	sf::Texture m_Texture;
+
+	float m_MaulTimer;
+	float m_PounceTimer;
+	float m_HP;
 };
